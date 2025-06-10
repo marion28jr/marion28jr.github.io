2dcd3689744ff5b860e0247ede6f99a89eaaca39
Marion Juré <marionjure@sfr.fr>
2025-06-10 09:02:58 +0200

    clean projet

--- exercices/src/App.tsx
+++ exercices/src/App.tsx
@@ -1,35 +1,65 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+interface Tab {
+  key: number;
+  title: string;
+  contents: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentTab, setCurrentTab] = useState<number>(1);
+  const exercisesTab: Tab[] = [
+    { key: 1, title: "Exercise 1", contents: "todo 1" },
+    { key: 2, title: "Exercise 2", contents: "todo 2" },
+    { key: 3, title: "Exercise 3", contents: "todo 3" },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="text-center">React Level 3 Certification Exam</h1>
+        </div>
+        <div className="col-12 mt-4">
+          <nav>
+            <div className="nav nav-tabs" role="tablist">
+              {exercisesTab.map((exerciseTab: Tab) => (
+                <button
+                  key={exerciseTab.key}
+                  className={`nav-link ${
+                    currentTab === exerciseTab.key ? "active" : ""
+                  }`}
+                  id={`nav-ex${exerciseTab.key}-tab`}
+                  data-bs-toggle="tab"
+                  data-bs-target={`#nav-ex${exerciseTab.key}`}
+                  type="button"
+                  role="tab"
+                  aria-controls={`nav-ex${exerciseTab.key}`}
+                  aria-selected={currentTab === exerciseTab.key}
+                  onClick={() => setCurrentTab(exerciseTab.key)}
+                >
+                  {exerciseTab.title}
+                </button>
+              ))}
+            </div>
+          </nav>
+          <div className="tab-content" id="nav-tabContent">
+            {exercisesTab.map((exerciseTab: Tab) => (
+              <div
+                className={`tab-pane fade ${
+                  currentTab === exerciseTab.key ? "show active" : ""
+                } mt-2`}
+                id={`nav-ex${exerciseTab.key}`}
+                role="tabpanel"
+                aria-labelledby={`nav-ex${exerciseTab.key}-tab`}
+              >
+                {exerciseTab.contents}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <h2>React niv 3</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
